71c9e60b1f1ee23d5732cc3d0e1ae80427871f4b
Danil Terentev <danil.terentyev@gmail.com>
2024-12-07 19:00:04 +0300

    Add files via upload

--- views.py
+++ views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
+
 
 # Create your views here.
+def index2(request):
+    return render(request, 'second_task/index2.html')
+
+
+class index(TemplateView):
+    def get(self, request):
+        return render(request, 'second_task/index.html')
